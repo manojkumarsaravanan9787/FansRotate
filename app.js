fa6528ccfda6aa259836ee777f0708857f2fe2b5
MANOJ KUMAR SARAVANAN <manojkumarsaravanan9787@gmail.com>
2025-03-22 16:22:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var on_btn = document.getElementById("on");
+var off_btn = document.getElementById("off");
+var img = document.getElementById("bulb");
+
+function bulb_on(){
+    img.src = "https://media.istockphoto.com/id/487004766/photo/ceiling-fan-blur.jpg?s=612x612&w=0&k=20&c=k8vkMoS1JnEfHRhN-RNXeN7jlfGjsgDi_4y-PZJ4rY0="
+}
+
+function bulb_off(){
+    img.src = "https://media.istockphoto.com/id/481593262/photo/spinning-ceiling-fan.jpg?s=612x612&w=0&k=20&c=EVzHm-FEmcSSvbHnhA2F3HPcBa5Ru4O2PeNSySm81yw="
+}
